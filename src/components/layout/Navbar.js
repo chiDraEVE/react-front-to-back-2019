e73fefa0d6c8287b584eb7ee718615212ee78a7c
chidraeve <chidraeve@clubitsolutions.de>
2019-12-27 19:57:22 +0100

    10. Components, Props & PropTypes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 
 class Navbar extends Component {
+
     render() {
         return (
             <nav className={"navbar bg-primary"}>
                 <h1>
-                    <i className={'fab fa-github'} />
-                       Navbar
+                    <i className={this.props.icon} />
+                       {this.props.title}
                 </h1>
             </nav>
         );
